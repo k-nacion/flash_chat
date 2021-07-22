98f399e0a305ca72bed6ca5c7a884e0fa1017f45
Kenn <kentot51810@gmail.com>
2021-07-22 09:49:36 +0800

    allow login with input keyboard

--- lib/widgets/text_input.dart
+++ lib/widgets/text_input.dart
@@ -1,38 +1,25 @@
 import 'package:flutter/material.dart';
 
-class TextFormInputField extends StatefulWidget {
-  final String? hint;
-  final TextInputType? keyboardType;
-  final String obscurringCharacter;
-  final bool? obscureText;
-  final TextEditingController? controller;
-  final String? Function(String?)? validator;
-
+class TextFormInputField extends TextFormField {
   TextFormInputField({
-    this.hint,
-    this.keyboardType,
-    this.obscureText,
-    this.obscurringCharacter = '•',
-    this.controller,
-    this.validator,
-  });
-
-  @override
-  _TextFormInputFieldState createState() => _TextFormInputFieldState();
-}
-
-class _TextFormInputFieldState extends State<TextFormInputField> {
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: widget.keyboardType,
-      obscuringCharacter: widget.obscurringCharacter,
-      controller: widget.controller,
-      obscureText: widget.obscureText ?? false,
-      decoration: InputDecoration(
-        hintText: widget.hint,
-      ),
-      validator: widget.validator,
-    );
-  }
+    Key? key,
+    String? hint,
+    TextInputType? keyboardType,
+    bool? obscureText,
+    String obscuringCharacter = '•',
+    TextEditingController? controller,
+    String? Function(String?)? validator,
+    void Function(String)? onFieldSubmitted,
+  }) : super(
+          key: key,
+          keyboardType: keyboardType,
+          obscuringCharacter: obscuringCharacter,
+          controller: controller,
+          obscureText: obscureText ?? false,
+          decoration: InputDecoration(
+            hintText: hint,
+          ),
+          validator: validator,
+          onFieldSubmitted: onFieldSubmitted,
+        );
 }
